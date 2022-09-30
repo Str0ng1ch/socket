fbe7bd350e848b9d6b66335d0b2ecc024a722999
Str0ng1ch <artemkuzmi@gmail.com>
2022-09-30 08:51:04 +0300

    Hot fix

--- client.py
+++ client.py
@@ -3,17 +3,17 @@ import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(('localhost', 3030))  # Подключаемся к нашему серверу.
+s.connect(('localhost', 3030))
 print('Соединение с сервером')
 time.sleep(1)
 
 print('Отправка данных серверу')
 time.sleep(1)
 
-s.sendall('Hello, Habr!'.encode('utf-8'))  # Отправляем фразу.
+s.sendall('Вот мое сообщение!'.encode('utf-8'))
 print('Прием данных от сервера')
 time.sleep(1)
 
-data = s.recv(1024)  # Получаем данные из сокета.
+data = s.recv(1024)
 s.close()
 print('Разрыв соединения с сервером')
--- server.py
+++ server.py
@@ -3,24 +3,24 @@ import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind(('localhost', 3030))  # Привязываем серверный сокет к localhost и 3030 порту.
+s.bind(('localhost', 3030))
 print('Запуск сервера')
 time.sleep(1)
 
-s.listen(1)  # Начинаем прослушивать входящие соединения.
+s.listen(1)
 print('Начало прослушивания порта')
 time.sleep(1)
 
-conn, addr = s.accept()  # Метод который принимает входящее соединение.
+conn, addr = s.accept()
 print('Подключение клиента')
 print('Прием данных от клиента')
 time.sleep(1)
 
 while True:
-    data = conn.recv(1024)  # Получаем данные из сокета.
+    data = conn.recv(1024)
     if not data:
         break
-    conn.sendall(data)  # Отправляем данные в сокет.
+    conn.sendall(data)
     print(data.decode('utf-8'))
     print('Отправка данных клиенту')
 conn.close()
